b55248e462db0ef54db0beeb295ec87b3e3b4e91
viniciusminas <viniciusminas_@hotmail.com>
2024-11-30 20:57:14 -0300

    last commit

--- config.php
+++ config.php
@@ -1,10 +1,9 @@
 <?php
 
-// Definindo as constantes de configuração do banco de dados
-define('DB_HOST', 'localhost');     // Endereço do servidor de banco de dados
-define('DB_PORT', '5432');          // Porta do banco de dados PostgreSQL (padrão)
-define('DB_NAME', 'TrabalhoSemestralplnb');  // Nome do banco de dados
-define('DB_USER', 'postgres');      // Nome do usuário para conexão
-define('DB_PASS', 'postgres');      // Senha do usuário
+define('DB_HOST', 'localhost');     
+define('DB_PORT', '5432');          
+define('DB_NAME', 'TrabalhoSemestralplnb'); 
+define('DB_USER', 'postgres');     
+define('DB_PASS', 'postgres');      
 
 ?>
